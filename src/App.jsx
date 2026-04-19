3c67710ba432a818a59869a4f56d4d1e6ff5092b
Aiman03at <aiman03at@gmail.com>
2026-04-19 17:47:02 -0400

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,7 @@ import { useState } from "react";
 import ResultScreen from './ResultScreen';
 import StartScreen from './StartScreen';
 import QuestionCard from './QuestionCard';
-const questions =[{
-  id:1,
-  question:"What does hTMl stand for?",
-  options:["Hyper Text Markup Language",
-    "Home Tool Markup Language",
-    "Hyperlinks and Text Markup Language",
-    "Hyperlinking Text Marking Language"
-
-  ],
-  correctAnswer:"Hyper Text Markup Language"
-},
-{
-  id:2,
-  question:"What does CSS stand for?",
-  options:["Creative Style Sheets",
-    "Cascading Style Sheets",
-    "Computer Style Sheets",
-    "Colorful Style Sheets"
-
-  ],
-  correctAnswer:"Cascading Style Sheets"
-},
-{
-  id:3,
-  question:"What does JS stand for?",
-  options:["JavaScript",
-    "JavaSource",
-    "JustScript",
-    "JScript"
-
-  ],
-  correctAnswer:"JavaScript"
-} 
-]
+import questions from './questions';
 
 
 function App() {
